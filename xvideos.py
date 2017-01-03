60546fc870391374da3b85f8f240670b360bec9f
Rey Merk <furbino1@gmail.com>
2017-01-03 16:57:35 +0100

    Automatic feed

--- xvideos.py
+++ xvideos.py
@@ -1,17 +1,57 @@
 import feedparser
 import telebot
+import thread
+import time
 
-bot = telebot.TeleBot('EH VOLEVI')
+bot = telebot.TeleBot('240238338:AAG6fgBFf_klr0erUWH2dmBMKSlT6t8k8Lw')
+
+def check():
+    while True:
+        time.sleep(20)
+        fappers_update()
+        feed_update()
+
+def fappers_update():
+    global ffile
+    global fappers
+    global fappersl
+    ffile = open('fappers.txt', 'r+')
+    fappers = ffile.read()
+    fappersl = fappers.split('\n')
+
+fappers_update()
 
 feed = feedparser.parse('http://www.xvideos.com/rss/rss.xml')
 
+def feed_update():
+    global feed
+    global feed2
+    feed2 = feedparser.parse('http://www.xvideos.com/rss/rss.xml')
+    if not feed2['entries'][1]['title'] == feed['entries'][1]['title']:
+        feed = feed2
+        if not fappers == '':
+            for element in fappersl:
+                if not element == '':
+                    bot.send_message(element, '<b>' + feed['entries'][1]['title'] + '</b> \n\n' + str(feed['entries'][1]['links'][0]['href']), parse_mode='HTML')
+
+thread.start_new_thread( check, ())
+
 def message_handler(messages):
+    global f
 
     for message in messages:
 
         if message.text:
 
             if message.text == '/start':
+                f = 0
+                for element in fappersl:
+                    if not message.from_user.id == element:
+                        f += 1
+
+                if f + 1 == len(fappersl):
+                    ffile = open('fappers.txt', 'a')
+                    ffile.write(str(message.from_user.id) + '\n')
                 bot.reply_to(message, 'Hey Fapper, thank you!' )
 
             if message.text == '/lastfeed':
@@ -19,4 +59,4 @@ def message_handler(messages):
 
 
 bot.set_update_listener(message_handler)
-bot.polling()
+bot.polling(none_stop=True)
